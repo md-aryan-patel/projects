04e852cef302b94a1782564895702ab7fad26742
Aryan Patel <aryan.patel@minddeft.net>
2023-02-17 18:45:12 +0530

    ICO contract improvement

--- contracts/ICO.sol
+++ contracts/ICO.sol
@@ -1,15 +1,17 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 error ICOStats(bytes message);
 
 contract ICO {
+
     uint256 counter;
+    uint256 decimals = 18;
 
     struct tokenProviders {
-        IERC20 token;
+        ERC20 token;
         address owner;
         uint256 amount;
         uint256 startTime;
@@ -17,67 +19,43 @@ contract ICO {
         uint256 pricePerToken;
     }
 
-    mapping (uint256 => tokenProviders) providers;
-    mapping (address => uint256) toProvider;
-    mapping (address => mapping(uint256 => uint256)) public investorsAmount;
-    mapping (address => mapping(address => uint256)) public providersAmount;
-    
+    mapping (uint256 => tokenProviders) public providers;
+
     function createIco(
         address _token,
         uint256 _amount,
         uint256 _startTime,
         uint256 _endTime, 
         uint256 _pricePerToken
-    ) public {
+    ) public returns(uint256){
         counter++;
-        require(_amount <= IERC20(_token).balanceOf(msg.sender) && _startTime > 0 && _endTime > _startTime && _pricePerToken != 0);
-        providers[counter] = tokenProviders(IERC20(_token), msg.sender, _amount, _startTime, _endTime, _pricePerToken);
-        IERC20(_token).approve(address(this), _amount);
-        toProvider[msg.sender] = counter;
+        require(/*_startTime >= block.timestamp && */ _endTime > _startTime && _pricePerToken != 0, "err:1");
+        providers[counter] = tokenProviders(ERC20(_token), msg.sender, _amount, _startTime, _endTime, _pricePerToken);
+        ERC20(_token).transferFrom(msg.sender,address(this), _amount);
+        return counter;
     }
 
-    function invest(uint256 icoNum, uint256 _tokenAmount) public {
-        if(block.timestamp < providers[icoNum].startTime) {
-            revert ICOStats({
-                message: "ICO not yet began"
-            });
-        }
-
-        if(block.timestamp > providers[icoNum].endTime) {
-            revert ICOStats({
-                message: "ICO has ended"
-            });
-        }
-        require(_tokenAmount > 0, "error: Purchase amount should be greater then 0");
-        require(providers[icoNum].owner != address(0), "error: ICO dosent exist");
-        require(providers[icoNum].amount != 0, "error: Tokens are sold");
-
-        uint256 transferAmount = providers[icoNum].pricePerToken * _tokenAmount;
+    function invest(uint256 icoNum) public payable returns(uint256){
+        tokenProviders memory current = providers[icoNum]; 
         
-        providersAmount[providers[icoNum].owner][msg.sender] += transferAmount;
-        investorsAmount[msg.sender][icoNum] += _tokenAmount;
-        providers[icoNum].amount -= _tokenAmount;
-    }
-
-    function withdraw(uint256 icoNum) public payable {
-        tokenProviders memory current = providers[icoNum];
+        require(block.timestamp >= current.startTime, "ICO not began yet");
+        require(block.timestamp <= current.endTime, "ICO ended");
+        uint256 tokenRequire = (msg.value / current.pricePerToken) * (10**18);
         
-        require(block.timestamp < current.startTime || block.timestamp > current.endTime, "Your ICO has began");
-        require(current.token.allowance(msg.sender, address(this)) <= investorsAmount[msg.sender][icoNum], "Withdrawal amount is too high");
-        require(investorsAmount[msg.sender][icoNum] > 0, "No tokens to withdraw");
-
-        current.token.transferFrom(current.owner, msg.sender, investorsAmount[msg.sender][icoNum]);
-        current.owner.call{value: providersAmount[current.owner][msg.sender]};
-        investorsAmount[msg.sender][icoNum] = 0;
-        providersAmount[current.owner][msg.sender] = 0;
+        if(tokenRequire > current.amount) {
+            uint256 tokenAmounInEth = (providers[icoNum].pricePerToken * current.amount) / (10**18);
+            uint256 transferAmount = msg.value - tokenAmounInEth;
+            payable(current.owner).transfer(transferAmount);
+            current.token.transfer(msg.sender, current.amount);
+            payable(msg.sender).transfer(msg.value - transferAmount);
+            current.amount = 0;
+        } else {
+            uint256 transferAmount = (providers[icoNum].pricePerToken * tokenRequire ) / (10**18);
+            require(msg.value >= transferAmount, "Err:fee not paid");
+            payable(providers[icoNum].owner).transfer(msg.value);
+            providers[icoNum].token.transfer(msg.sender, tokenRequire);
+            current.amount = current.amount - tokenRequire;
+        }
+        return current.amount;
     }
-
-    function getTimestamp() public view returns(uint256) {
-        return block.timestamp + 120;
-    }
-
-    function getTimestamp2() public view returns(uint256) {
-        return block.timestamp + 240;
-    }
-
 }
